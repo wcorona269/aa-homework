caeb431a2bf9d186faa5599f1fe3c63ce0274f95
wcorona269 <wcorona269@gmail.com>
2022-04-12 00:02:41 -0400

    finished homework

--- w11d2/joblisting_homework/frontend/reducer.js
+++ w11d2/joblisting_homework/frontend/reducer.js
@@ -6,13 +6,16 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   // return state; // remove this and fill out the body of the reducer function
   switch(action.type) {
-    case "SWITCH_LOCATION";
-      return [...state, action.jobs];
+    case "SWITCH_LOCATION":
+      // return [...state, action.jobs];
+      return {
+        city: action.city,
+        jobs: action.jobs
+      }
     default:
         return state;
 
   }
 };
 
-window.reducer = reducer;
 export default reducer;
